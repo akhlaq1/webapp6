b9d44619efebc832a3865db91d704e58a80f42bb
Akhlaq <engr.akhlaq@hotmail.com>
2023-06-03 20:41:19 +0500

    JS started

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Variable
+
+// What is a variable
+// How to declare a variable
+
+// Data Types
+
+// 1. String --> "Hello World"
+
+// 2. Number --> parseInt ( changes string to number ) --> Integer & Float
+
+// 3. Boolean  ( 1 or 0 | True or False | yes or no )
+
+// 4
